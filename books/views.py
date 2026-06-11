e5f796a25af03af17e8b0e9e29bc864a214bf69c
agapouyi <pouya.zm83@gmail.com>
2026-06-11 15:43:42 +0330

    update style of templates and add search bar

--- books/views.py
+++ books/views.py
@@ -1,19 +1,35 @@
 # books/views.py
-from django.contrib.auth.mixins import LoginRequiredMixin,PermissionRequiredMixin# new
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.views.generic import ListView, DetailView
+from django.db.models import Q
 from .models import Book
 
 
-class BookListView(LoginRequiredMixin, ListView): # new
+class BookListView(LoginRequiredMixin, ListView):
     model = Book
     context_object_name = 'book_list'
     template_name = 'books/book_list.html'
-    login_url = 'account_login' # new
+    login_url = 'account_login'
 
-class BookDetailView(PermissionRequiredMixin,LoginRequiredMixin, DetailView): # new
+
+class BookDetailView(PermissionRequiredMixin, LoginRequiredMixin, DetailView):
     model = Book
     context_object_name = 'book'
     template_name = 'books/book_detail.html'
-    login_url = 'account_login' # new
+    login_url = 'account_login'
     permission_required = 'books.special_status'
+
+
+class SearchResultsListView(ListView):
+    model = Book
+    context_object_name = 'book_list'
+    template_name = 'books/search_results.html'
+
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        if query:
+            return Book.objects.filter(
+                Q(title__icontains=query) | Q(author__icontains=query)
+            )
+        return Book.objects.all()
     
